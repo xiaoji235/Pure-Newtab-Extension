e5634e99f7795be3b52f34bdabda63fcd9edb95f
xjtx <73387366+xiaoji235@users.noreply.github.com>
2021-12-25 12:44:26 +0800

    Delete js directory

--- js/popup.js
+++
@@ -1,5 +0,0 @@
-window.onload=function(){
-    const manifest = chrome.runtime.getManifest();
-    const version = manifest.version;
-    versionBox.innerText = version;
-}
--- js/yiyan.js
+++
@@ -1,1 +0,0 @@
-var hitokoto="https://v1.hitokoto.cn";var hitText=document.getElementById("hitokoto");var xhr=new XMLHttpRequest();xhr.open("get","https://v1.hitokoto.cn");xhr.onreadystatechange=function(){var data=JSON.parse(xhr.responseText);hitText.innerHTML="「&nbsp;&nbsp;"+data.hitokoto+"」"+"<br><br>——"+data.from};xhr.send();
