4de3b1af494aed069904f81ed1d6e79e8ed4d294
xjtx <73387366+xiaoji235@users.noreply.github.com>
2022-01-09 15:23:47 +0800

    新增使用离线壁纸功能

--- js/background.js
+++ js/background.js
@@ -18,7 +18,16 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("save-backimg").addEventListener("click", savebackimg);
 });
 document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("save-backimg64").addEventListener("click", savebackimg64);
+});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("local-bg").addEventListener("click", localbg);
+});
+document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("close-set").addEventListener("click", closeset);
+});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("close-localbg").addEventListener("click", closelocalbg);
 });
 
 function openset(){
@@ -36,9 +45,35 @@ function savebackimg(){
 	window.location.reload()
 }
 
+function savebackimg64(){
+	document.getElementById("settingpage").style="display:block;opacity:0"
+	setTimeout(function(){document.getElementById("settingpage64").style="display:none"},200)
+	localStorage.setItem("backurl",document.getElementById("conte").value)
+	alert("保存成功！")
+	window.location.reload()
+}
+
+function localbg(){
+	document.getElementById("settingpage2").style="display:block;opacity:0"
+	setTimeout(function(){document.getElementById("settingpage2").style="display:block;opacity:1"},100)
+}
+
 function closeset(){
 	document.getElementById("settingpage").style="display:block;opacity:0"
 	setTimeout(function(){document.getElementById("settingpage").style="display:none"},100)
 	document.getElementById("setbg").style="display:block;opacity:0"
 	setTimeout(function(){document.getElementById("setbg").style="display:none;opacity:0"},100)
+}
+
+function closelocalbg(){
+	document.getElementById("settingpage2").style="display:block;opacity:0"
+	setTimeout(function(){document.getElementById("settingpage2").style="display:none"},100)
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("start").addEventListener("click", yulan);
+});
+function yulan(){
+	document.getElementById("imgShow").style="opacity:1"
 }
